05b27a4ee27ed9c57816adb677f575ff3077a1d3
David Chouinard <david.chouinard@gmail.com>
2023-02-20 11:16:48 -0500

    ajoute de source et correction des route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Ajouter from './Ajouter';
+import BlogDetail from './BlogDetail';
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="contenu">
+          <Routes>
+            <Route path='/' element={<Home/>}>
+            </Route>
+            <Route path='/ajouter'element={<Ajouter/>}>
+            </Route>
+            <Route path='/blogs/:id' element={<BlogDetail/>}>
+            </Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
